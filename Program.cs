d241f444e34a90616a225d8f803ba487bf2398b3
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-06-27 20:42:36 -0300

    Atividade Finalizada

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿List<int> numeros = new List<int>();
+int numeroDigitado;
+int contador = 0;
 
+Console.Clear();
 
+Console.Write("Escreva um número: ");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Dígitos: ");
+
+do
+{
+numeros.Add(numeroDigitado % 10);
+numeroDigitado /= 10;
+
+Console.Write($"{numeros.ElementAt(contador)} ");
+contador++;
+} while (numeroDigitado != 0);
+
+Console.WriteLine($"\n\nSoma: {numeros.Sum()}\n");
